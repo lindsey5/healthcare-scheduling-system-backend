9e34ff960f7c7855af80db25fde4255e56c279ee
lindseysamson5 <lindseysamson5@gmail.com>
2026-08-21 19:09:48 +0800

    fix: remove unnecessary line in message model for clarity

--- src/models/index.ts
+++ src/models/index.ts
@@ -59,7 +59,6 @@ Conversation.belongsTo(Staff, { foreignKey: "assignedStaffId", as: "staff" });
 
 Message.belongsTo(Conversation, { foreignKey: "conversationId", as: "conversation" });
 Message.belongsTo(Patient, { foreignKey: "senderId", as: "patientSender", constraints: false });
-
 Message.belongsTo(Staff, { foreignKey: "senderId", as: "staffSender", constraints: false });
 
 export { 
